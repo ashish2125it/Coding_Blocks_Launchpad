ad4d7743e92036e0ebd3e74027f6aac13f71a05c
ashish2125it <khuranaashish816@gmail.com>
2025-04-22 18:58:13 +0530

    Create Pogram1.cpp

---
+++ Coding-Blocks-Launchpad-main/Lacture3/Pogram1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    // Taking input from user
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    
+    for (int i = 1; i <= n; ++i) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
